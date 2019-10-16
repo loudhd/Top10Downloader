8b7029864e988b9f87f0f1fc04d3566d2c4fb531
acer <wongsatorn1501003@gmail.com>
2019-10-16 22:13:17 +0700

    10

--- app/src/main/java/com/example/top10downloader/MainActivity.kt
+++ app/src/main/java/com/example/top10downloader/MainActivity.kt
@@ -36,48 +36,50 @@ class MainActivity : AppCompatActivity() {
             override fun doInBackground(vararg url: String?): String {
                 Log.d(TAG, "doInBackground: starts with ${url[0]}")
                 val rssFeed = downloadXML(url[0])
-                if (rssFeed.isEapty()){
+                if (rssFeed.isEmpty()){
                     Log.d(TAG,"doInBackground:Eror downloading")
                 }
                 return rssFeed
             }
-        }
-    }
-    private fun downloadXML(urlPath: String?): String {
-        val xmlResult = StringBuilder()
+            private fun downloadXML(urlPath: String?): String {
+                val xmlResult = StringBuilder()
 
-        try {
-            val url = URL(urlPath)
-            val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
-            val response = connection.responseCode
-            Log.d(TAG, "downloadXML: The response code was $response")
+                try {
+                    val url = URL(urlPath)
+                    val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+                    val response = connection.responseCode
+                    Log.d(TAG, "downloadXML: The response code was $response")
 
-           // val inputStream = connection.inputStream
-            //val inputStreamReader = InputStreamReader(inputStream)
-           // val reader = BufferedReader(inputStreamReader)
-            val reader = BufferedReader(InputStreamReader(connection.inputStream))
+                    // val inputStream = connection.inputStream
+                    //val inputStreamReader = InputStreamReader(inputStream)
+                    // val reader = BufferedReader(inputStreamReader)
+                    val reader = BufferedReader(InputStreamReader(connection.inputStream))
 
-            val inputBuffer = CharArray(500)
-            var CharRead = 0
-            while (CharRead>= 0 ){
-                CharRead = reader.read(inputBuffer)
-                if (CharRead > 0){
-                    xmlResult.append(String(inputBuffer,0,CharRead ))
+                    val inputBuffer = CharArray(500)
+                    var CharRead = 0
+                    while (CharRead>= 0 ){
+                        CharRead = reader.read(inputBuffer)
+                        if (CharRead > 0){
+                            xmlResult.append(String(inputBuffer,0,CharRead ))
+                        }
+                    }
+                    reader.close()
+
+                    Log.d(TAG,"Receivved ${xmlResult.length} bytes")
+                    return xmlResult.toString()
+
+                } catch (e: MalformedURLException) {
+                    Log.e(TAG, "downloadXML: Invalid URL ${e.message}")
+                } catch (e: IOException) {
+                    Log.e(TAG, "downloadXML: IO Exception reading data: ${e.message}")
+                } catch (e: Exception) {
+                    Log.e(TAG, "Unknown error: ${e.message}")
                 }
+                return ""
             }
-            reader.close()
 
-            Log.d(TAG,"Receivved ${xmlResult.length} bytes")
-            return xmlResult.toString()
-
-        } catch (e: MalformedURLException) {
-            Log.e(TAG, "downloadXML: Invalid URL ${e.message}")
-        } catch (e: IOException) {
-            Log.e(TAG, "downloadXML: IO Exception reading data: ${e.message}")
-        } catch (e: Exception) {
-            Log.e(TAG, "Unknown error: ${e.message}")
         }
-        return ""
+
     }
 
 
