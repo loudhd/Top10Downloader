8030c36adbdbfc52aee6c490c165e482b174377c
acer <wongsatorn1501003@gmail.com>
2019-10-17 17:33:31 +0700

    34

--- app/src/main/java/com/example/top10downloader/MainActivity.kt
+++ app/src/main/java/com/example/top10downloader/MainActivity.kt
@@ -2,17 +2,16 @@ package com.example.top10downloader
 
 import android.content.Context
 import android.os.AsyncTask
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.ArrayAdapter
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ListView
+import androidx.annotation.RequiresApi
+import androidx.constraintlayout.widget.ConstraintLayout
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.MalformedURLException
 import java.net.URL
 import kotlin.properties.Delegates
 
@@ -36,24 +35,58 @@ class FeedEntry {
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
 
+    @RequiresApi(Build.VERSION_CODES.CUPCAKE)
     private  val downloadData = DownloadData(this, xmlListView)
 
+    @RequiresApi(Build.VERSION_CODES.CUPCAKE)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d(TAG, "onCreate called")
-        downloadData.execute("http://ax.itumes.appie.com/WebObjects/M25toreServices.woa/ws/RSS/topfreeapplications/limit=10/xml")
+        downloadUrl("http://ax.itumes.appie.com/WebObjects/M25toreServices.woa/ws/RSS/topfreeapplications/limit=10/xml")
         Log.d(TAG, "onCreate: done")
     }
+    @RequiresApi(Build.VERSION_CODES.CUPCAKE)
+    private fun downloadUrl(feedURL: String){
+        Log.d(TAG,"downlaodUrl starting AsyncTask")
+        downloadData.execute(feedURL)
+        Log.d(TAG,"downloadUrl done")
+    }
 
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return super.onCreateOptionsMenu(menu)
+        return  true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val feedUrl: String
+        when(item.itemId){
+            R.id.mnuFree->
+                feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml"
+            R.id.mnuPaid->
+                feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=10/xml"
+            R.id.mnuSongs->
+                feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=10/xml"
+            else ->
+                return super.onOptionsItemSelected(item)
+        }
+        downloadData(feedUrl)
+        return true
+    }
+
+    @RequiresApi(Build.VERSION_CODES.CUPCAKE)
     override fun onDestroy() {
         super.onDestroy()
         downloadData.cancel(true)
     }
 
     companion object {
-        private class DownloadData(context: Context,ListView: ListView) : AsyncTask<String, Void, String>() {
+        @RequiresApi(Build.VERSION_CODES.CUPCAKE)
+        private class DownloadData(
+            context: Context,
+            ListView: ConstraintLayout
+        ) : AsyncTask<String, Void, String>() {
             private val TAG = "DownloadData"
             var propContext:Context by Delegates.notNull()
             var propListView: ListView by Delegates.notNull()
@@ -63,14 +96,12 @@ class MainActivity : AppCompatActivity() {
                 propListView = ListView
             }
 
+            @RequiresApi(Build.VERSION_CODES.CUPCAKE)
             override fun onPostExecute(result: String) {
                 super.onPostExecute(result)
-                //Log.d(TAG, "onPostExecute: parameter is $result")
                 val parseApplications = ParseApplications()
                 parseApplications.parse(result)
 
-               // val arrayAdaptor = ArrayAdapter<FeedEntry>(propContext,R.layout.list_item, parseApplications.applications)
-               // propListView.adapter = arrayAdaptor
                 val feedAdapter = FeedAdapter(propContext, R.layout.list_item,parseApplications.applications)
                 propListView.adapter = feedAdapter
             }
